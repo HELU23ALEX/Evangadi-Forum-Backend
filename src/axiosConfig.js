76899c6fd2603add99e3c08f44401ab353b4198b
HELU23ALEX <HELINAAALEMU@gmail.com>
2024-06-23 00:48:07 +0300

    deploy

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL:"http://localhost:5500/api"
-  // baseURL: "https://helina-evangadi.cyclic.app/api",
-  baseURL:'http://localhost:5500/api'
+  // baseURL:'http://localhost:5500/api'
+  baseURL: "https://evangadi-forum-backend-2-ddph.onrender.com/",
 });
 
 export default axiosBase;
