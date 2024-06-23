0006d7844e413a19022f103dd8608c3232759fd4
HELU23ALEX <HELINAAALEMU@gmail.com>
2024-06-23 23:36:54 +0300

    nothing

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const axiosBase = axios.create({
   // baseURL:"http://localhost:5500/api"
   // baseURL:'http://localhost:5500/api'
-  baseURL: "http://evangadi-forum-backend-2-ddph.onrender.com/",
+  // baseURL: "http://evangadi-forum-backend-2-ddph.onrender.com/",
+  baseURL: "https://evangadi-forum-backend-2-ddph.onrender.com",
 });
 
 export default axiosBase;
