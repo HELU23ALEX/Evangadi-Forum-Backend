8d592c464c16059e0011139d9fcf60097bca71ef
HELU23ALEX <HELINAAALEMU@gmail.com>
2024-06-24 00:49:46 +0300

    api added

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,7 +4,7 @@ const axiosBase = axios.create({
   // baseURL:"http://localhost:5500/api"
   // baseURL:'http://localhost:5500/api'
   // baseURL: "http://evangadi-forum-backend-2-ddph.onrender.com/",
-  baseURL: "https://evangadi-forum-backend-2-ddph.onrender.com",
+  baseURL: "https://evangadi-forum-backend-2-ddph.onrender.com/api",
 });
 
 export default axiosBase;
