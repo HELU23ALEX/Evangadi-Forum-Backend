147bf02c80142f4db01b2755b2be221cdceca2bf
HELU23ALEX <HELINAAALEMU@gmail.com>
2024-06-24 11:06:55 +0300

    backend attached

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5500/api",
-  // baseURL: "http://evangadi-forum-backend-2-ddph.onrender.com/",
-  // baseURL: "https://evangadi-forum-backend-2-ddph.onrender.com/api",
+  // baseURL: "http://localhost:5500/api",
+  baseURL: "https://evangadi-forum-backend-2-ddph.onrender.com/api",
 });
 
 export default axiosBase;
-
-
-
-
-// https://spontaneous-chebakia-464120.netlify.app
